b0a419795021698d89d1eb6af38367176d62a609
Ekaterna <edbelova@outlook.com>
2025-10-16 15:31:39 -0700

    Add book routes and model for managing book data

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # Register Blueprints here
+    app.register_blueprint(books_bp)
     
     return app
