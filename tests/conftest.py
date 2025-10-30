4e2d1d50c7760ee40b2176e684a1c69341bb5c74
Ekaterna <edbelova@outlook.com>
2025-10-29 21:25:09 -0700

    Add fixture for two saved books and update test for retrieving one book

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app import create_app
 from app.db import db
+from app.models.book import Book
 from flask.signals import request_finished
 from dotenv import load_dotenv
 import os
@@ -26,7 +27,14 @@ def app():
     with app.app_context():
         db.drop_all()
 
-
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_saved_books(app):
+    ocean_book = Book(title="Ocean Book", description="watr 4evr")
+    mountain_book = Book(title="Mountain Book", description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    db.session.commit()
