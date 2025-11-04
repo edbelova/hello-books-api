e7007067b8f1eb2ca69e3e569c363c593567acb5
Ekaterna <edbelova@outlook.com>
2025-11-03 16:52:03 -0800

    Add author relationship to Book model and create migration for foreign key

--- app/models/book.py
+++ app/models/book.py
@@ -1,19 +1,25 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional
 from ..db import db
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+    author: Mapped[Optional["Author"]] = relationship(back_populates="books")
 
     @classmethod
     def from_dict(cls, book_data):
         return cls(title=book_data["title"],
-                   description=book_data["description"])
+                   description=book_data["description"],
+                   author_id=book_data.get("author_id"))
 
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
-            "description": self.description
+            "description": self.description,
+            "author_id": self.author_id
         }
